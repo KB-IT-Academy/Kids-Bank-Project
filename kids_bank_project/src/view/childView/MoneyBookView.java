c36e179728b21874f7eb95dd79791084c7ab49c2
YunseoKang <rkddbstj00@naver.com>
2023-08-03 03:23:31 +0900

    fix: ChildView.MoneyBookView, ParentView.MoneyBookView 수정

--- kids_bank_project/src/view/childView/MoneyBookView.java
+++ kids_bank_project/src/view/childView/MoneyBookView.java
@@ -2,22 +2,25 @@ package view.childView;
 
 import java.util.Scanner;
 
+/**
+ * 용돈기입장 View
+ */
 public class MoneyBookView {
-
 	static Scanner sc = new Scanner(System.in);
 
-	public MoneyBookView() {
-	}
+	public MoneyBookView() {}
 
+	/**
+	 * 용돈기입장 메뉴 화면 출력하는 메소드
+	 */
 	static public void printMoneyBookMenu() {
-
 		System.out.println("----------------------------------------------------------------------------------------------");
 		System.out.println("                                         용 돈 기 입 장                                          ");
 		System.out.println("----------------------------------------------------------------------------------------------");
 		
 		System.out.println();
 		System.out.println("**********************************************************************************************");
-		System.out.println("          1.조회          2.등록          3.수정          4.삭제  ");
+		System.out.println("                      1.조회          2.등록          3.수정          4.삭제                       ");
 		System.out.println("----------------------------------------------------------------------------------------------");
 		System.out.print("메뉴선택 >> ");
 
@@ -37,19 +40,19 @@ public class MoneyBookView {
 			break;
 		default:
 			System.out.println("화면에 보이는 메뉴 번호를 입력해주세요.");
-
 		}
 	}
-
+	
+	/**
+	 * 용돈기입장 조회 화면 출력하는 메소드
+	 */
 	static public void printSearchMenu() {
-		
 		System.out.println();
 		System.out.println("----------------------------------------------------------------------------------");
-		System.out.println("           1. 최근 5건     2. 날짜 조회     3. 월별 조회   4. 전체 조회   ");
+		System.out.println("           1. 최근 5건     2. 날짜 조회     3. 월별 조회   4. 전체 조회                     ");
 		System.out.print("메뉴선택 >> ");
 
 		String menu = sc.nextLine();
-
 		switch (menu) {
 		case "1":
 
@@ -65,141 +68,128 @@ public class MoneyBookView {
 			break;
 		default:
 			System.out.println("화면에 보이는 메뉴 번호를 입력해주세요.");
-
 		}
 	}
 
+	/**
+	 * 용돈기입장 등록 화면 출력하는 메소드
+	 */
 	static public void printInsertMoneyBook() {
-
 		System.out.println();
 		System.out.println("----------------------------------------------------------------------------------");
-		System.out.println("작성할 날짜 입력: ");
-		
-		//에러처리+날짜 내역들 출력 메소드
-		
+		System.out.println("작성할 날짜 입력 : ");
 		String moneydate = sc.nextLine();
 		
-		System.out.println("1. 수입     2. 지출  ");
-		System.out.print("메뉴선택 >> ");
-
+		System.out.println("1. 수입 2. 지출");
+		System.out.print("분류 선택 : ");
 		String moneyType = sc.nextLine();
 		
-		int amount;
-		String content;
-		String memo;
-		int outComeType;
+		int outComeType, amount;
+		String content, memo;
 		
 		switch (moneyType) {
 		case "1":
-			System.out.println("금액: ");
+			System.out.println("금액 입력 : ");
 			amount = Integer.parseInt(sc.nextLine());
-			System.out.println("내용: ");
+			System.out.println("내용 입력 : ");
 			content = sc.nextLine();
-			System.out.println("메모: ");
+			System.out.println("메모 입력 : ");
 			memo = sc.nextLine();
 			//기능구현
 			break;
 		case "2":
-			System.out.println("지출 분류 선택>> 1.식비 2.간식 3.교통비 4.문화생활 5.기념일 6.기타");
+			System.out.println("1.식비 2.간식 3.교통비 4.문화생활 5.기념일 6.기타");
+			System.out.println("지출 분류 선택 : ");
 			outComeType = Integer.parseInt(sc.nextLine());
-			System.out.println("금액: ");
+			System.out.println("금액 입력 : ");
 			amount = Integer.parseInt(sc.nextLine());
-			System.out.println("내용: ");
+			System.out.println("내용 입력 : ");
 			content = sc.nextLine();
-			System.out.println("메모: ");
+			System.out.println("메모 입력 : ");
 			memo = sc.nextLine();
 			//기능구현
 			break;
 		default:
 			System.out.println("화면에 보이는 메뉴 번호를 입력해주세요.");
-
 		}
-
 	}
 
+	/**
+	 * 용돈기입장 수정 화면 출력하는 메소드
+	 */
 	static public void printUpdateMoneyBook() {
 		System.out.println();
 		System.out.println("----------------------------------------------------------------------------------");
-		System.out.println("수정할 날짜 입력: ");
-		
-		
-		String moneydate = sc.nextLine();
-		
+		System.out.println("수정할 날짜 입력 : ");
+		String moneyDate = sc.nextLine();
 		//에러처리+날짜 내역들 출력 메소드
 		
-		System.out.println("수정할 번호 입력 (수정할 내역이 없으면 -1 입력) : ");
+		System.out.println("수정할 번호 입력(수정할 내역 없으면 -1 입력) : ");
 		String rownum = sc.nextLine();
 		
 		if(rownum.equals("-1")) {
 			return;
 		}else {
 			selectContentOfBook();
-			
 		}
-
 	}
 
+	/**
+	 * 용돈기입장 삭제 화면 출력하는 메소드
+	 */
 	static public void printDeleteMoneyBook() {
-
 		System.out.println();
 		System.out.println("----------------------------------------------------------------------------------");
-		System.out.println("삭제할 날짜 입력: ");
-		
-		
-		String moneydate = sc.nextLine();
-		
+		System.out.println("삭제할 날짜 입력 : ");
+		String moneyDate = sc.nextLine();
 		//에러처리+날짜 내역들 출력 메소드
 		
-		System.out.println("삭제할 번호 입력 (삭제할 내역이 없으면 -1 입력) : ");
+		System.out.println("삭제할 번호 입력(삭제할 내역 없으면 -1 입력) : ");
 		String exit = sc.nextLine();
 		
 		if(exit.equals("-1")) {
 			return;
 		}else {
-			
 			//rownum int형으로 변환 뒤 메소드 실행
 			//내역 번호 안누르면 에러처리
 		}
-		
 	}
 	
+	/**
+	 * 용돈기입장 수정(상세 수정) 화면 출력하는 메소드
+	 */
 	static private void selectContentOfBook() {
-		
 		System.out.println();
 		System.out.println("----------------------------------------------------------------------------------");
-		System.out.println("수정할 내역 선택 : 1.금액  2.내용  3.메모 ");
-		
-		System.out.print("메뉴선택 >> ");
+		System.out.println("1.금액 2.내용 3.메모");
+		System.out.print("수정할 항목 선택 : ");
 
 		String menu = sc.nextLine();
 		switch (menu) {
 		case "1":
-			System.out.println("수정할 금액: ");
+			System.out.println("수정할 금액 입력 : ");
 			int amount = Integer.parseInt(sc.nextLine());
 			//rownum int형으로 변환 뒤 메소드 실행
 			//내역 번호 안누르면 에러처리
 			
-			System.out.println("수정 완료되었습니다.");
 			break;
 		case "2":
-			System.out.println("수정할 내용: ");
+			System.out.println("수정할 내용 입력 : ");
 			String content = sc.nextLine();
 			//rownum int형으로 변환 뒤 메소드 실행
 			//내역 번호 안누르면 에러처리
 			
-			System.out.println("수정 완료되었습니다.");
 			break;
 		case "3":
-			System.out.println("수정할 메모: ");
+			System.out.println("수정할 메모 입력: ");
 			String memo = sc.nextLine();
 			//rownum int형으로 변환 뒤 메소드 실행
 			//내역 번호 안누르면 에러처리
 			
-			System.out.println("수정 완료되었습니다.");
 			break;
 		default:
 			System.out.println("화면에 보이는 메뉴 번호를 입력해주세요.");
-
 		}
+		System.out.println("수정 완료되었습니다.");
 	}
 }
--- kids_bank_project/src/view/parentView/MoneyBookView.java
+++ kids_bank_project/src/view/parentView/MoneyBookView.java
@@ -2,22 +2,25 @@ package view.parentView;
 
 import java.util.Scanner;
 
+/**
+ * 용돈기입장 View
+ */
 public class MoneyBookView {
-
 	static Scanner sc = new Scanner(System.in);
 
-	public MoneyBookView() {
-	}
+	public MoneyBookView() {}
 
+	/**
+	 * 용돈기입장 메뉴 화면 출력하는 메소드
+	 */
 	static public void printMoneyBookMenu() {
-
 		System.out.println("----------------------------------------------------------------------------------------------");
 		System.out.println("                                         용 돈 기 입 장                                          ");
 		System.out.println("----------------------------------------------------------------------------------------------");
 		
 		System.out.println();
 		System.out.println("**********************************************************************************************");
-		System.out.println("                         1.조회          2.댓글 기능         3.좋아요 기능");
+		System.out.println("                         1.조회          2.댓글 기능         3.좋아요 기능                           ");
 		System.out.println("----------------------------------------------------------------------------------------------");
 		System.out.print("메뉴선택 >> ");
 
@@ -27,26 +30,26 @@ public class MoneyBookView {
 			printSearchMenu();
 			break;
 		case "2":
-			printInsertCommentsMoneyBook();
+			printCommentsMoneyBookMenu();
 			break;
 		case "3":
-			printInsertLikeMoneyBook();
+			printLikeMoneyBookMenu();
 			break;
 		default:
 			System.out.println("화면에 보이는 메뉴 번호를 입력해주세요.");
-
 		}
 	}
 
+	/**
+	 * 용돈기입장 조회 화면 출력하는 메소드
+	 */
 	static public void printSearchMenu() {
-		
 		System.out.println();
-		System.out.println("----------------------------------------------------------------------------------------------");
-		System.out.println("                     1. 최근 5건     2. 날짜 조회     3. 월별 조회   4. 전체 조회   ");
+		System.out.println("----------------------------------------------------------------------------------");
+		System.out.println("           1. 최근 5건     2. 날짜 조회     3. 월별 조회   4. 전체 조회                     ");
 		System.out.print("메뉴선택 >> ");
 
 		String menu = sc.nextLine();
-
 		switch (menu) {
 		case "1":
 
@@ -62,12 +65,13 @@ public class MoneyBookView {
 			break;
 		default:
 			System.out.println("화면에 보이는 메뉴 번호를 입력해주세요.");
-
 		}
 	}
-
-	static public void printInsertCommentsMoneyBook() {
-		
+	
+	/**
+	 * 용돈기입장 댓글 화면 출력하는 메소드
+	 */
+	static public void printCommentsMoneyBookMenu() {
 		//날짜가 잘 등록하면
 		System.out.println();
 		System.out.println("----------------------------------------------------------------------------------------------");
@@ -76,17 +80,18 @@ public class MoneyBookView {
 
 		String menu = sc.nextLine();
 
-		String date;
-		String content;//댓글
+		String moneyDate;
+		String content; //댓글
 		String rownum;
+		
 		switch (menu) {
 		case "1":
 			System.out.println();
 			System.out.println("----------------------------------------------------------------------------------");
-			System.out.println("댓글 등록할 날짜 입력: ");		
-			date = sc.nextLine();
+			System.out.println("댓글 작성할 날짜 입력 : ");		
+			moneyDate = sc.nextLine();
 			//날짜가 알맞게 들어오면
-			System.out.println("등록한 댓글 입력: ");
+			System.out.println("댓글 내용 입력 : ");
 			content = sc.nextLine();
 			//댓글 등록 기능 실행
 			System.out.println("댓글 등록 완료되었습니다.");
@@ -94,16 +99,16 @@ public class MoneyBookView {
 		case "2":
 			System.out.println();
 			System.out.println("----------------------------------------------------------------------------------");
-			System.out.println("댓글 수정할 날짜 입력: ");		
-			date = sc.nextLine();
+			System.out.println("댓글 수정할 날짜 입력 : ");		
+			moneyDate = sc.nextLine();
 			//날짜가 알맞게 들어오면 날짜에 따른 댓글들 출력(rownum사용)
-			System.out.println("수정할 번호 입력 (수정할 내역이 없으면 -1 입력) : ");
+			System.out.println("수정할 번호 입력(수정할 내역 없으면 -1 입력) : ");
 			rownum = sc.nextLine();
 			
 			if(rownum.equals("-1")) {
 				return;
 			}else {
-				System.out.println("수정할 댓글 내용: ");
+				System.out.println("수정할 댓글 내용 입력 : ");
 				content = sc.nextLine();
 				//rownum int형으로 변환 뒤 메소드 실행
 				//내역 번호 안누르면 에러처리
@@ -116,9 +121,9 @@ public class MoneyBookView {
 			System.out.println();
 			System.out.println("----------------------------------------------------------------------------------");
 			System.out.println("댓글 삭제할 날짜 입력: ");		
-			date = sc.nextLine();
+			moneyDate = sc.nextLine();
 			//날짜가 알맞게 들어오면 날짜에 따른 댓글들 출력(rownum사용)
-			System.out.println("삭제할 번호 입력 (수정할 내역이 없으면 -1 입력) : ");
+			System.out.println("삭제할 번호 입력(삭제할 내역이 없으면 -1 입력) : ");
 			rownum = sc.nextLine();
 			
 			if(rownum.equals("-1")) {
@@ -132,57 +137,55 @@ public class MoneyBookView {
 			break;
 		default:
 			System.out.println("화면에 보이는 메뉴 번호를 입력해주세요.");
-
 		}
-
 	}
-
-	static public void printInsertLikeMoneyBook() {
+	
+	/**
+	 * 용돈기입장 좋아요 화면 출력하는 메소드
+	 */
+	static public void printLikeMoneyBookMenu() {
 		//날짜가 잘 등록하면
-				System.out.println();
-				System.out.println("----------------------------------------------------------------------------------------------");
-				System.out.println("                              1.좋아요 등록     2. 좋아요 삭제    ");
-				System.out.print("메뉴선택 >> ");
+		System.out.println();
+		System.out.println("----------------------------------------------------------------------------------------------");
+		System.out.println("                              1.좋아요 등록     2. 좋아요 삭제                                       ");
+		System.out.print("메뉴선택 >> ");
 
-				String menu = sc.nextLine();
+		String menu = sc.nextLine();
 
-				String date;
-				String emoji;//좋아요,싫어요
-				String rownum;
-				switch (menu) {
-				case "1":
-					System.out.println();
-					System.out.println("----------------------------------------------------------------------------------");
-					System.out.println("좋아요 등록할 날짜 입력: ");		
-					date = sc.nextLine();
-					//날짜가 알맞게 들어오면
-					//댓글 등록 기능 실행
-					System.out.println("좋아요 등록 완료되었습니다.");
-					break;
-				case "2":
-					System.out.println();
-					System.out.println("----------------------------------------------------------------------------------");
-					System.out.println("좋아요 삭제할 날짜 입력: ");		
-					date = sc.nextLine();
-					//날짜가 알맞게 들어오면 날짜에 따른 댓글들 출력(rownum사용)
-					System.out.println("삭제할 번호 입력 (수정할 내역이 없으면 -1 입력) : ");
-					rownum = sc.nextLine();
-					
-					if(rownum.equals("-1")) {
-						return;
-					}else {
-						//rownum int형으로 변환 뒤 메소드 실행
-						//내역 번호 안누르면 에러처리
-						
-						System.out.println("삭제 완료되었습니다.");
-					}
-					break;
-				default:
-					System.out.println("화면에 보이는 메뉴 번호를 입력해주세요.");
-
-				}
-
+		String moneyDate;
+		String emoji; //좋아요,싫어요
+		String rownum;
+		
+		switch (menu) {
+		case "1":
+			System.out.println();
+			System.out.println("----------------------------------------------------------------------------------");
+			System.out.println("좋아요 등록할 날짜 입력 : ");		
+			moneyDate = sc.nextLine();
+			//날짜가 알맞게 들어오면
+			//댓글 등록 기능 실행
+			System.out.println("좋아요 등록 완료되었습니다.");
+			break;
+		case "2":
+			System.out.println();
+			System.out.println("----------------------------------------------------------------------------------");
+			System.out.println("좋아요 삭제할 날짜 입력 : ");		
+			moneyDate = sc.nextLine();
+			//날짜가 알맞게 들어오면 날짜에 따른 댓글들 출력(rownum사용)
+			System.out.println("삭제할 번호 입력(수정할 내역 없으면 -1 입력) : ");
+			rownum = sc.nextLine();
+			
+			if(rownum.equals("-1")) {
+				return;
+			}else {
+				//rownum int형으로 변환 뒤 메소드 실행
+				//내역 번호 안누르면 에러처리
+				
+				System.out.println("삭제 완료되었습니다.");
+			}
+			break;
+		default:
+			System.out.println("화면에 보이는 메뉴 번호를 입력해주세요.");
+		}
 	}
-	
-	
 }
