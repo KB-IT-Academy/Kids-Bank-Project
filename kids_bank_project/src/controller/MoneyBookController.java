9f47334ceb061c20d13febc3a2844af539bec809
98nob <98noble@naver.com>
2023-08-03 19:13:10 +0900

    add:

--- kids_bank_project/src/controller/MoneyBookController.java
+++ kids_bank_project/src/controller/MoneyBookController.java
@@ -3,16 +3,21 @@ package controller;
 import java.util.List;
 
 import dto.MoneyBookDto;
-
+import service.MoneyBookService;
+import service.MoneyBookServiceImpl;
+import view.FailView;
+import view.SuccessView;
 
 public class MoneyBookController{
+	
+	private static MoneyBookService moneyBookService = MoneyBookServiceImpl.getInstance();
 	/**
 	 * 기입장 생성하는 메서드
 	 * @param MoneyBookdto
 	 * @return total_amount(잔액)
 	 */
 	public static void createMoneyBook(MoneyBookDto dto) {
-		
+		moneyBookService.createMoneyBook(dto);
 	}
 	
 	/**
@@ -20,14 +25,14 @@ public class MoneyBookController{
 	 * @param MoneyBookDto
 	 */
 	public static void updateMoneyBook(MoneyBookDto dto) {
-		
+		moneyBookService.updateMoneyBook(dto);
 	}
 	
 	/**
 	 * 자신의 전체 기입장을 삭제하는 메서드
 	 */
 	public static void deleteAllMoneyBook() {
-		
+		moneyBookService.deleteAllMoneyBook();
 	}
 	
 	/**
@@ -35,14 +40,14 @@ public class MoneyBookController{
 	 * @param 기입장 row number 
 	 */
 	public static void deleteMoneyBook(int rownum) {
-		
+		moneyBookService.deleteMoneyBook(rownum);
 	}
 	
 	/**
 	 * 작성한 전체 기입장을 조회하는 메서드
 	 */
 	public static void getAllMoneyBook() {
-		
+		 List<MoneyBookDto> list = moneyBookService.getAllMoneyBook();
 	}
 	
 	/**
@@ -50,7 +55,7 @@ public class MoneyBookController{
 	 * @param 날짜 조회 => 입력 값 "2023-08-03"
 	 */
 	public static void getDayMoneyBook(String date) {
-		
+		List<MoneyBookDto> list = moneyBookService.getDayMoneyBook(date);
 	}
 	
 	/**
@@ -58,7 +63,7 @@ public class MoneyBookController{
 	 * @param 한달 조회 => 입력 값 "2023-08"
 	 */
 	public static void getMonthMoneyBook(String date) {
-	
+		List<MoneyBookDto> list = moneyBookService.getMonthMoneyBook(date);
 	}
 	
 	/**
@@ -66,7 +71,7 @@ public class MoneyBookController{
 	 * @return
 	 */
 	public static void getRecentMoneyBook() {
-		
+		List<MoneyBookDto> list = moneyBookService.getRecentMoneyBook();
 	}
 	
 }	
