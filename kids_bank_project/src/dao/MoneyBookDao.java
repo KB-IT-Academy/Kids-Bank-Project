bb0e0abd219707b70e9ae907a78f75cb66d254f6
98nob <98noble@naver.com>
2023-08-03 18:38:25 +0900

    add : moneybook

--- kids_bank_project/src/dao/MoneyBookDao.java
+++ kids_bank_project/src/dao/MoneyBookDao.java
@@ -3,6 +3,7 @@ package dao;
 import java.util.List;
 
 import dto.MoneyBookDto;
+import dto.UserDto;
 
 public interface MoneyBookDao {
 	/**
@@ -10,48 +11,49 @@ public interface MoneyBookDao {
 	 * @param MoneyBookdto
 	 * @return total_amount(잔액)
 	 */
-	int createMoneyBook(MoneyBookDto dto);
+	public int createMoneyBook(MoneyBookDto dto);
 	
 	/**
 	 * 기입장을 수정하는 메서드
 	 * @param MoneyBookDto
 	 */
-	void updateMoneyBook(MoneyBookDto dto);
+	public int updateMoneyBook(MoneyBookDto dto);
 	
 	/**
 	 * 자신의 전체 기입장을 삭제하는 메서드
 	 */
-	void deleteAllMoneyBook();
+	public int deleteAllMoneyBook();
 	
 	/**
 	 * rownum을 활용하여 기입장을 삭제하는 메서드(rownum으로 고유번호 확인)
 	 * @param 기입장 row number 
 	 */
-	void deleteMoneyBook(int rownum);
+	public int deleteMoneyBook(int rownum);
 	
 	/**
 	 * 작성한 전체 기입장을 조회하는 메서드
 	 * @return
 	 */
-	List<MoneyBookDto> getAllMoneyBook();
+	public List<MoneyBookDto> getAllMoneyBook();
 	
 	/**
 	 * 지정한 날짜의 기입장 조회하는 메서드
 	 * @param 날짜 조회 => 입력 값 "2023-08-03"
 	 * @return
 	 */
-	List<MoneyBookDto> getDayMoneyBook(String date);
+	public List<MoneyBookDto> getDayMoneyBook(String date);
 	
 	/**
 	 * 기입장 한달 분을 조회하는 메서드
 	 * @param 한달 조회 => 입력 값 "2023-08"
 	 * @return
 	 */
-	List<MoneyBookDto> getMonthMoneyBook(String date);
+	public List<MoneyBookDto> getMonthMoneyBook(String date);
 	
 	/**
 	 * 최근 5개의 기입장을 조회하는 메서드
 	 * @return
 	 */
-	List<MoneyBookDto> getRecentMoneyBook();
+	public List<MoneyBookDto> getRecentMoneyBook();
+
 }
--- kids_bank_project/src/dao/MoneyBookDaoImpl.java
+++ kids_bank_project/src/dao/MoneyBookDaoImpl.java
@@ -1,7 +1,13 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import common.DBManager;
 import dto.MoneyBookDto;
 
 public class MoneyBookDaoImpl implements MoneyBookDao{
@@ -14,43 +20,186 @@ private static MoneyBookDao instance = new MoneyBookDaoImpl();
 	}
 	@Override
 	public int createMoneyBook(MoneyBookDto dto) {
-		// TODO Auto-generated method stub
+		Connection con = null;
+		PreparedStatement ps= null;
+		int result = 0;
+		String sql = "insert into board (money_book_num,  child_num, money_type, export_type, amount, content, memo, money_date, write_date, update_date from money_book values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		try {
+			con = DBManager.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, dto.getMoneyBookNum());
+			//static 으로 자식 고유번호 가져오기
+			ps.setInt(2, 1);
+			ps.setInt(3, dto.getMoneyType());
+			ps.setString(4, dto.getOutcomeType());
+			ps.setInt(5, dto.getAmount());
+			ps.setString(6, dto.getContent());
+			ps.setString(7, dto.getMemo());
+			ps.setString(8, dto.getMoney_date());
+			ps.setString(9, dto.getWrite_date());
+			ps.setString(10, dto.getUpdate_date());
+		
+			result = ps.executeUpdate();
+			int amount = dto.getAmount();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally {
+			DBManager.releaseConnection(con, ps);
+		}
 		return 0;
 	}
 	@Override
-	public void updateMoneyBook(MoneyBookDto dto) {
-		// TODO Auto-generated method stub
+	public int updateMoneyBook(MoneyBookDto dto) {
 		
+		Connection con = null;
+		PreparedStatement ps= null;
+		int result = 0;
+		String sql = "update board set content = ? from money_book where money_date = ?";
+		try {
+			con = DBManager.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setString(1, dto.getContent());
+			ps.setString(2, dto.getMoney_date());
+		
+			result = ps.executeUpdate();
+			
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally {
+			DBManager.releaseConnection(con, ps);
+		}
+		return result;
 	}
+	
 	@Override
-	public void deleteAllMoneyBook() {
-		// TODO Auto-generated method stub
+	public int deleteAllMoneyBook() {
+		Connection con = null;
+		PreparedStatement ps = null;
+		int result = 0;
+		String sql = "delete money_book";
+		try {
+			con = DBManager.getConnection();
+			ps = con.prepareStatement(sql);
+			result = ps.executeUpdate();
+			
+		}catch(SQLException e){
+			e.printStackTrace();
+			DBManager.releaseConnection(con, ps);
+		}
+		return result;
 		
 	}
-	@Override
-	public void deleteMoneyBook(int rownum) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 	@Override
 	public List<MoneyBookDto> getAllMoneyBook() {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		List<MoneyBookDto> list = new ArrayList<>();
+		String sql = "select rownum, money_book_num, money_type, export_type, amount, content, memo, money_date, write_date, update_date from money_book order by money_date ";
+		try {
+			con = DBManager.getConnection();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				list.add(new MoneyBookDto(rs.getInt("rownum"), rs.getInt("money_book_num"), rs.getInt("money_type") , rs.getString( "export_type"), rs.getInt(" amount"), rs.getString("content"), rs.getString(" memo"), rs.getString(" money_date"), rs.getString( "write_date"), rs.getString ("update_date")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.releaseConnection(con, ps, rs);
+		}
+		
+		return list;
 	}
+	
 	@Override
 	public List<MoneyBookDto> getDayMoneyBook(String date) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		List<MoneyBookDto> list = new ArrayList<>();
+		String sql = "select rownum, money_book_num, money_type, export_type, amount, content, memo, money_date, write_date, update_date from money_book where money_date = ? order by money_date ";
+		try {
+			con = DBManager.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setString(1, date);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				list.add(new MoneyBookDto(rs.getInt("rownum"), rs.getInt("money_book_num"), rs.getInt("money_type") , rs.getString( "export_type"), rs.getInt(" amount"), rs.getString("content"), rs.getString(" memo"), rs.getString(" money_date"), rs.getString( "write_date"), rs.getString ("update_date")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.releaseConnection(con, ps, rs);
+		}
+		
+		return list;
 	}
 	@Override
 	public List<MoneyBookDto> getMonthMoneyBook(String date) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		List<MoneyBookDto> list = new ArrayList<>();
+		String sql = "select rownum, money_book_num, money_type, export_type, amount, content, memo, money_date, write_date, update_date from money_book where substr(money_date,0,6) = ? order by money_date ";
+		try {
+			con = DBManager.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setString(1, date);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				list.add(new MoneyBookDto(rs.getInt("rownum"), rs.getInt("money_book_num"), rs.getInt("money_type") , rs.getString( "export_type"), rs.getInt(" amount"), rs.getString("content"), rs.getString(" memo"), rs.getString(" money_date"), rs.getString( "write_date"), rs.getString ("update_date")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.releaseConnection(con, ps, rs);
+		}
+		
+		return list;
 	}
 	@Override
 	public List<MoneyBookDto> getRecentMoneyBook() {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		List<MoneyBookDto> list = new ArrayList<>();
+		String sql = "select rownum, money_book_num, money_type, export_type, amount, content, memo, money_date, write_date, update_date from money_book order by money_date ";
+		try {
+			con = DBManager.getConnection();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			for(int i = 0; i<5; i++) {
+				if (rs.next()) {
+					list.add(new MoneyBookDto(rs.getInt("rownum"), rs.getInt("money_book_num"), rs.getInt("money_type") , rs.getString( "export_type"), rs.getInt(" amount"), rs.getString("content"), rs.getString(" memo"), rs.getString(" money_date"), rs.getString( "write_date"), rs.getString ("update_date")));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.releaseConnection(con, ps, rs);
+		}
+		
+		return list;
+	}
+	
+	@Override
+	public int deleteMoneyBook(int rownum) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		int result = 0;
+		String sql = "delete * from money_book where rownum = ? ";
+		try {
+			con = DBManager.getConnection();
+			ps = con.prepareStatement(sql);
+			result = ps.executeUpdate();
+			
+		}catch(SQLException e){
+			e.printStackTrace();
+			DBManager.releaseConnection(con, ps);
+		}
+		return result;
 	}
 
 }
