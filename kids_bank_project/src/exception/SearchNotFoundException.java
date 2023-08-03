26cc97145a07e42bd1277bc414a62ed7841ca64f
WoongKi1115 <nwk6638@gmail.com>
2023-08-03 18:14:42 +0900

    add: exceptions

--- kids_bank_project/src/exception/SearchNotFoundException.java
+++ kids_bank_project/src/exception/SearchNotFoundException.java
@@ -1,5 +1,16 @@
 package exception;
 
-public class SearchNotFoundException {
-
+/**
+ * insert, update, delete 에 결과가 오류가 있을때 발생시킬예외
+ * */
+public class SearchNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public SearchNotFoundException() {}
+	
+	
+	public SearchNotFoundException(String message) {
+		super(message);
+	}
+	
 }
