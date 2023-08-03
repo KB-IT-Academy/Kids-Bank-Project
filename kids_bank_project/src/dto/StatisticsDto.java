e3eb163e42be9dd78bfb25b894987823234491c9
YunseoKang <rkddbstj00@naver.com>
2023-08-04 04:27:33 +0900

    fix: StatisticsDto 수정

--- kids_bank_project/src/dto/StatisticsDto.java
+++ kids_bank_project/src/dto/StatisticsDto.java
@@ -1,7 +1,7 @@
 package dto;
 
 public class StatisticsDto {
-	private String id;
+	private int id;
 	private int inComeAmount;
 	private int outComeAmount;
 	private String startDate;
@@ -13,49 +13,76 @@ public class StatisticsDto {
 	 */
 	public StatisticsDto() {}
 	
-	public StatisticsDto(String nickname, int inComeAmount, int outComeAmount,
+	public StatisticsDto(int id, int inComeAmount, int outComeAmount,
 			String startDate, String endDate, CategoryDto categoryDto) {
-		super();
-		this.categoryDto = categoryDto;
+		this.id = id;
 		this.inComeAmount = inComeAmount;
 		this.outComeAmount = outComeAmount;
 		this.startDate = startDate;
 		this.endDate = endDate;
+		this.categoryDto = categoryDto;
 	}
+	
 	/**
 	 * getter, setter
 	 */
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public int getInComeAmount() {
 		return inComeAmount;
 	}
+
 	public void setInComeAmount(int inComeAmount) {
 		this.inComeAmount = inComeAmount;
 	}
+
 	public int getOutComeAmount() {
 		return outComeAmount;
 	}
+
 	public void setOutComeAmount(int outComeAmount) {
 		this.outComeAmount = outComeAmount;
 	}
+
 	public String getStartDate() {
 		return startDate;
 	}
+
 	public void setStartDate(String startDate) {
 		this.startDate = startDate;
 	}
+
 	public String getEndDate() {
 		return endDate;
 	}
+
 	public void setEndDate(String endDate) {
 		this.endDate = endDate;
 	}
+
+	public CategoryDto getCategoryDto() {
+		return categoryDto;
+	}
+
+	public void setCategoryDto(CategoryDto categoryDto) {
+		this.categoryDto = categoryDto;
+	}
+	
 	/**
 	 * 통계 toString 
 	 */
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("StatisticsDto [inComeAmount=");
+		builder.append("StatisticsDto [id=");
+		builder.append(id);
+		builder.append(", inComeAmount=");
 		builder.append(inComeAmount);
 		builder.append(", outComeAmount=");
 		builder.append(outComeAmount);
@@ -63,13 +90,9 @@ public class StatisticsDto {
 		builder.append(startDate);
 		builder.append(", endDate=");
 		builder.append(endDate);
+		builder.append(", categoryDto=");
+		builder.append(categoryDto);
 		builder.append("]");
 		return builder.toString();
 	}
-	
-	
-	
-	
-	
-	
 }
