3fd1ec04bff5e6323bafb0ad16663c3c8477d95e
WoongKi1115 <nwk6638@gmail.com>
2023-08-04 12:58:49 +0900

    add: emoji create, delete

--- kids_bank_project/src/dto/EmojiDto.java
+++ kids_bank_project/src/dto/EmojiDto.java
@@ -8,21 +8,17 @@ public class EmojiDto {
 	private int emojiType; // 이모지타입(좋아요:0, 싫어요:1)
 	private String moneyDate; // 이모지 등록할 수입일 or 지출일
 
-	private String write_date; // 등록일
-	private String update_date; // 수정일
 	
 	/**
 	 * 이모지 생성자
 	 */
 	public EmojiDto() {}
 
-	public EmojiDto(ParentDto parentDto, ChildDto childDto, int emojiType, String moneyDate, String write_date, String update_date) {
+	public EmojiDto(ParentDto parentDto, ChildDto childDto, int emojiType, String moneyDate) {
 		this.parentDto = parentDto;
 		this.childDto = childDto;
 		this.emojiType = emojiType;
 		this.moneyDate = moneyDate;
-		this.write_date = write_date;
-		this.update_date = update_date;
 	}
 
 	/**
@@ -60,22 +56,6 @@ public class EmojiDto {
 		this.moneyDate = moneyDate;
 	}
 
-	public String getWrite_date() {
-		return write_date;
-	}
-
-	public void setWrite_date(String write_date) {
-		this.write_date = write_date;
-	}
-
-	public String getUpdate_date() {
-		return update_date;
-	}
-
-	public void setUpdate_date(String update_date) {
-		this.update_date = update_date;
-	}
-
 	/**
 	 * 이모지 toString
 	 */
@@ -87,8 +67,6 @@ public class EmojiDto {
 		sb.append(", childNum=").append(childDto.getChildNum());
 		sb.append(", emojiType=").append(emojiType);
 		sb.append(", moneyDate='").append(moneyDate).append('\'');
-		sb.append(", write_date='").append(write_date).append('\'');
-		sb.append(", update_date='").append(update_date).append('\'');
 		sb.append('}');
 		return sb.toString();
 	}
--- kids_bank_project/src/service/CommentEmojiService.java
+++ kids_bank_project/src/service/CommentEmojiService.java
@@ -2,27 +2,36 @@ package service;
 
 import java.util.List;
 
+import dao.ChildDao;
+import dao.ChildDaoImpl;
+import dao.CommentEmojiDao;
+import dao.CommentEmojiDaoImpl;
 import dto.CommentDto;
 import dto.EmojiDto;
+import exception.DMLException;
 
-public interface CommentEmojiService {
+public class CommentEmojiService {
+	static CommentEmojiDao commentEmojiDao = new CommentEmojiDaoImpl();  
 	/**
 	 * 댓글 생성하는 메서드
 	 * @param dto
 	 */
-	void createComment(CommentDto dto);
+	public void createComment(int childNum, int parentNum, String content, String date) throws DMLException {
+		
+		
+	}
 	
 	/**
 	 * 댓글 수정하는 메서드
 	 * @param dto
 	 */
-	void updateComment(CommentDto dto);
+	public void updateComment(CommentDto dto) {};
 	
 	/**
 	 * 댓글 삭제하는 메서드
 	 * @param 선택할 날짜
 	 */
-	void deleteComment(String date);
+	public void deleteComment(String date) {};
 	
 	/**
 	 * 자식 고유 번호와 해당 날짜로 댓글들 조회하는 메서드
@@ -30,19 +39,33 @@ public interface CommentEmojiService {
 	 * @param moneyDate 댓글 단 날짜
 	 * @return
 	 */
-	List<CommentDto> getComment(int num, String moneyDate);
+	List<CommentDto> getComment(int num, String moneyDate) {
+		return null;
+	};
 	
 	/**
 	 * 이모지 생성하는 메서드
 	 * @param EmojiDto
 	 */
-	void craeteEmoji(EmojiDto dto);
+	public int craeteEmoji(int childNum, int parentNum, String content, String date) throws DMLException {
+		int result = commentEmojiDao.craeteEmoji(childNum, parentNum, content, date);
+		if (result == 0) {
+			throw new DMLException("이모지를 생성할 수 없습니다.");
+		}
+		return result;
+	};
 	
 	/**
 	 * 해당 날짜와 자기 자신(부모)의 num 값으로 이모지 삭제하는 메서드
 	 * @param moneyDate
 	 */
-	public void deleteEmoji(String moneyDate);
+	public int deleteEmoji(int num, String date) {
+		int result = commentEmojiDao.deleteEmoji(num, date);
+		if (result == 0) {
+			throw new DMLException("이모지를 삭제할 수 없습니다.");
+		}
+		return result;
+	};
 	
 	/**
 	 * 자식 고유번호와 찾을 날짜를 활용하여 이모지를 조회하는 메서드
@@ -50,5 +73,7 @@ public interface CommentEmojiService {
 	 * @param moneyDate : 찾을 날짜
 	 * @return
 	 */
-	List<EmojiDto> getEmoji(int num, String moneyDate);
+	List<EmojiDto> getEmoji(int num, String moneyDate) {
+		return null;
+	};
 }
